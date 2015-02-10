65944305094aa127500d095a321e227e29adbb5a
Tyler Roach <tyler.roach@sparcedge.com>
2015-02-10 14:01:22 -0500

    update for android studio v1

--- app/build/generated/source/buildConfig/debug/com/tylerjroach/camPreviewSave/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/tylerjroach/camPreviewSave/BuildConfig.java
@@ -5,7 +5,7 @@ package com.tylerjroach.camPreviewSave;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.tylerjroach.camPreviewSave";
+  public static final String APPLICATION_ID = "com.tylerjroach.camPreviewSave";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 33;
